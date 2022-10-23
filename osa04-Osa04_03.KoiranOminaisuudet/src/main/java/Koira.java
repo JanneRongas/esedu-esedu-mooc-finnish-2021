4b1d35f0956797aea61273fd4f545e1277c94b33
JanneRongas <113413887+JanneRongas@users.noreply.github.com>
2022-10-23 12:26:44 +0300

    Revert "Revert "Osat 1-7""
    This reverts commit d743cb5dc5fe7678adc164b4b7e2ec87434a6d00.

--- osa04-Osa04_03.KoiranOminaisuudet/src/main/java/Koira.java
+++
@@ -1,6 +0,0 @@
-public class Koira {
-    private String nimi;
-    private String rotu;
-    private int ika;
-    
-}
--- osa04-Osa04_06.Ovi/src/main/java/Ovi.java
+++
@@ -1,7 +0,0 @@
-public class Ovi {
-
-    public void koputa() {
-        System.out.println("Who's there?");
-        
-    }
-}
